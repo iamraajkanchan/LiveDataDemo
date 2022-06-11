780af96cb47a73042ed426bbe4fdedc61526b762
Raaj Kanchan <iamraajkanchan@gmail.com>
2022-06-11 12:36:42 +0530

    Updated on 11th June 2022

--- app/src/main/java/com/chinkyfamily/livedatademo/UserInfoActivity.kt
+++ app/src/main/java/com/chinkyfamily/livedatademo/UserInfoActivity.kt
@@ -3,6 +3,9 @@ package com.chinkyfamily.livedatademo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chinkyfamily.livedatademo.databinding.ActivityUserInfoBinding
+import com.chinkyfamily.livedatademo.fragments.UserDetail
+
+const val USER_DETAIL_FRAGMENT : String = "user detail fragment"
 
 /**
  * UserInfoActivity
@@ -20,5 +23,9 @@ class UserInfoActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         _binding = ActivityUserInfoBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        val userDetailFragment = UserDetail.newInstance();
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fcvUserInfo , userDetailFragment , USER_DETAIL_FRAGMENT)
+            .commitAllowingStateLoss()
     }
 }
